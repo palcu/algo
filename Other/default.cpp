f7de696c0e6179a5ce24824ed8806ea82bba9ffa
Alex Palcuie <alex.palcuie@gmail.com>
2011-11-26 16:29:26 +0200

    added other and license

---
+++ Other/default.cpp
@@ -0,0 +1,49 @@
+/***************************************************
+ * Author: Alexandru Palcuie
+ * Country: Romania
+ * Email: alex [dot] palcuie [at] gmail [dot] com
+ * Website: http://palcu.blogspot.com/
+ * Year: 2011
+****************************************************/
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+//Constants
+
+//Global Vars
+
+//Structs
+
+//Solve Functions
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("code.in","r",stdin);
+    freopen("code.out","w",stdout);
+    #endif
+    
+    
+	
+    return 0;
+}
