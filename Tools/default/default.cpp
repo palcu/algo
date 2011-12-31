797d805c5d9034eff9dd7c0e04dc4120b0b4abfd
Alex Palcuie <alex.palcuie@gmail.com>
2011-12-31 18:33:11 +0200

    new project template and script creator

---
+++ Tools/default/default.cpp
@@ -0,0 +1,83 @@
+/***************************************************
+ * Author: Alexandru Palcuie
+ * Country: Romania
+ * Email: alex [dot] palcuie [at] gmail [dot] com
+ * Website: http://palcu.blogspot.com/
+ * Year: 2012
+****************************************************/
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <fstream>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+#include <cstring>
+
+using namespace std;
+
+//Define
+#define NAME(n) #n
+#define pr(x) db((x), (#x))
+#define prv(v) dbv((v), (#v), (n))
+#define prw(v) dbw((v), (#v))
+
+//Constants
+const int N = 1<<10;
+
+//Structs
+
+//Global Vars
+int n, v[N];
+
+//{Helpers
+template <typename T>
+inline void db(const T x, const char * name){
+	cerr << name << " = " << x << '\n';
+}
+template <typename T> inline void dbv(const T * v, const char * name, int n){
+	fprintf(stderr, "=== %s ===\n", name);
+	for(int i=0; i<n; i++)
+		cerr << v[i] << " ";
+	cerr << '\n';
+}
+template <typename T> inline void dbs(T x){
+	cerr << x << ' ';
+}
+
+template<typename T>
+void dbw(const std::vector<T>& t, const char * name){
+	fprintf(stderr, "=== %s ===\n", name);
+	unsigned n = t.size();
+	for(typename std::vector<T>::size_type i=0; i<n; ++i)
+		std::cerr << t[i] << ' ';
+	cerr << '\n';
+}
+//}
+
+//Solve Functions
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("default.in","r",stdin);
+	//freopen("default.out","w",stdout);
+	#endif
+	
+	
+	
+	return 0;
+}
